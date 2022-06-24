87a2dba1696f947d81057105726ca9d22c8b011a
Megi <Megi@DESKTOP-K8OIC6D>
2022-06-24 07:07:33 +0300

    Add project files.

---
+++ WebDriverTestsDemo/WikipediaTest.cs
@@ -0,0 +1,25 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+//Създаване на нов Хром Браузър
+var driver = new ChromeDriver();
+
+//Навигиране до Уикипедия
+driver.Url = "https://wikipedia.org";
+
+System.Console.WriteLine("CURRENT TITLE: " + driver.Title);
+
+//Локирате на Search елемента по ID
+var searchField = driver.FindElement(By.Id("searchInput"));
+
+//Click на елемента
+searchField.Click();
+
+//Попълване с QA и натискане на Enter
+searchField.SendKeys("QA" + Keys.Enter);
+
+System.Console.WriteLine("TITLE AFTER SEARCH: " + driver.Title);
+
+// Затваряне на браузъра
+driver.Quit();
